a7fa96b2f9d8abd82bfdba1d6b573e9194924396
karan jain <karanjain2212@gmail.com>
2023-02-01 18:05:05 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@
 import './App.css';
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculator';
+import Navbar from './components/navbar';
+import Home from './components/home';
+import Quote from './components/quote';
 
 class App extends React.Component {
   render() {
-    return (<Calculator />);
+    return (
+      <>
+        <React.StrictMode>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route exact path="/calculator" element={<Calculator />} />
+              <Route exact path="/" element={<Home />} />
+
+              <Route path="/quote" element={<Quote />} />
+            </Routes>
+          </Router>
+        </React.StrictMode>
+      </>
+    );
   }
 }
 
---
+++ src/components/quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './quote.css';
+
+export default function Quote() {
+  return (
+    <div className=" pageContainer">
+      <p style={{ fontFamily: 'Tangerine', fontSize: '2.5em' }}>
+        &quot;Mathematics is not about numbers, equations, computations, or
+        algorithms: it is about understanding.&quot;
+      </p>
+      <span style={{ fontFamily: 'Tangerine', fontSize: '1.5em' }}>- Willian Paul Thurtson</span>
+    </div>
+  );
+}
